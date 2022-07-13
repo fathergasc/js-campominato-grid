db6da6b24e92414c4abd5bdbb6688c4fd9e476bb
Andrea Quaglioni <quaglioniandrea@gmail.com>
2022-07-13 18:02:41 +0200

    add: difficulty levels tied to number of squares, tied squares width to total number of squares; added {once:true} to eventListener to prevent infinite creation of squares

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
-const containerDom = document.getElementById('container');
-const playButton = document.getElementById('play');
+const containerDom = document.getElementById("container");
+const playButton = document.getElementById("play");
 
-playButton.addEventListener('click', 
-    createProgressiveNumberedBox(100)
+playButton.addEventListener(
+  "click",
+  function () {
+    const difficulty = document.getElementById("difficulty").value;
+    console.log("difficulty: ", typeof difficulty);
+    const difficultyN = parseInt(difficulty);
+    console.log("difficultyN: ", typeof difficultyN);
+
+    createToggableProgressiveNumberedBox(difficultyN);
+  },
+  { once: true }
 );
 
-
-function createProgressiveNumberedBox(number) {
-    
-    for (let i = 1; i<=number; i++) {
-    const addBox = document.createElement('div');  
+function createToggableProgressiveNumberedBox(number) {
+  for (let i = 1; i <= number; i++) {
+    const addBox = document.createElement("div");
     addBox.className = "box";
+    if (difficulty.value == '81') {
+        addBox.classList.add("box9");
+    } else if (difficulty.value == '49') {
+        addBox.classList.add("box7");
+    } else if (difficulty.value == '100') {
+        addBox.classList.add("box10");
+    }
     containerDom.append(addBox);
     addBox.append(i);
-    }
+    addBox.addEventListener("click", function () {
+      addBox.classList.toggle("active");
+    });
+  }
 }
-    
